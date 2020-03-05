94ee039c9eb3c9214efb98a710a949562496319b
zshen <zshen@videye.net>
2020-03-05 15:37:17 -0800

    1216: correct usage of 'memo[]'

--- zshen/dp/1216.leetcode.valid-palindrome-3.py
+++ zshen/dp/1216.leetcode.valid-palindrome-3.py
@@ -4,8 +4,11 @@ leet code: 超时的版本：
 class Solution(object):
     def helper(self, s, i, j, k, memo):
         i_,j_ = i,j
-        if memo[i][j]:
-            return True
+        if memo[i][j]>0:
+            if memo[i][j] == 1:
+                return True
+            elif memo[i][j] == 2:
+                return False
         while i<j:
             if s[i] == s[j]:
                 i+=1
@@ -15,16 +18,16 @@ class Solution(object):
                 return self.helper(s, i+1, j, k-1,memo) or self.helper(s,i,j-1,k-1,memo)
             else:
                 # not equal, and have used all 'K' allowance
-                memo[i_][j_] = False
+                memo[i_][j_] = 2
                 return False        
-        memo[i_][j_] = True
+        memo[i_][j_] = 1
         return True
     
     def isValidPalindrome(self, s, k):
         n = len(s)
-        memo = [[False]*n for _ in range(n)]
+        memo = [[0]*n for _ in range(n)]
         return self.helper(s, 0, len(s)-1, k,memo)
-		
+
 """
 1. [done] add helper 
 2. [done] change left, right => i,j 
