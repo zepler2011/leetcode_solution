5b8daa9126d069a45bbab73e498c984a6f51bcbf
zshen <zshen@videye.net>
2020-03-06 21:47:51 -0800

    Add DFS without return value

--- zshen/dp-dfs/word-break-2.lc582.dfs+memo.py
+++ zshen/dp-dfs/word-break-2.lc582.dfs+memo.py
@@ -33,3 +33,33 @@ class Solution:
 """
 TODO: 1. pass partition as an argument;
 """
+class Solution:
+    """
+    @param: s: A string
+    @param: wordDict: A set of words.
+    @return: All possible sentences.
+    """
+    def wordBreak(self, s, wordDict):
+        # write your code here
+        results = []
+        self.dfs(s, wordDict, 0, [], results)
+        for r in results:
+            print r
+        return results 
+
+    def dfs(self, s, dict, start, candidates, res):
+        n = len(s)
+        print start, n, candidates 
+        if start == n-1:
+            res.append(candidates)
+            return
+
+        for i in range(start,n):
+            prefix = s[start:i+1]
+            print prefix 
+            if prefix not in dict:
+                continue
+            candidates.append(prefix)
+            self.dfs(s, dict, start+i, candidates, res)
+            candidates.pop()
+        return
