bb828b9cf9aeaf96398b48ff6e23c49e8207bf8a
zshen <zshen@videye.net>
2020-03-07 14:17:38 -0800

    word-break-2 dfs+df: add DP helper. But chao shi

--- zshen/dp-dfs/word-break-2.lc582.dfs+dp.py
+++ zshen/dp-dfs/word-break-2.lc582.dfs+dp.py
@@ -7,21 +7,42 @@ class Solution:
     def wordBreak(self, s, wordDict):
         # write your code here
         results = []
-        self.dfs(s, wordDict, 0, [], results)
+        dp = self.calc(s, wordDict)
+        self.dfs(s, wordDict, 0, [], results, dp)
         return results 
 		
-    def dfs(self, s, dict, start, candidates, res):
+    def dfs(self, s, dict, start, candidates, res, dp):
         n = len(s)
-        #print start, candidates
         if start == n:
             res.append(' '.join(candidates))
             return
 		
         for i in range(start,n+1):
+            if i<n and not dp[i]:
+                continue 
             prefix = s[start:i]
             if prefix not in dict or prefix == '':
                 continue
             candidates.append(prefix)
-            self.dfs(s, dict, i, candidates, res)
+            self.dfs(s, dict, i, candidates, res, dp)
             candidates.pop()
         return
+
+    def calc(self, s, dict):
+        # write your code here
+        n = len(s)
+        f = [False] * (n+1)
+        f[0] = True
+        
+        for i in range(1, n+1):
+            for j in range(i):
+                f[i] = f[j] and s[j:i] in dict
+                if f[i]:
+                    break 
+                
+        return f
+"""
+1. compare with return value edition 
+2. Add DP
+3. string slicing
+"""
