b7d7eaa73592f17282c28f82005c9abea9f2627c
zshen <zshen@videye.net>
2020-03-18 17:21:18 -0700

    sudoku-solver-lc802: fix grid init

--- zshen/dfs-combi-permu/sudoku-solver-lc802.py
+++ zshen/dfs-combi-permu/sudoku-solver-lc802.py
@@ -4,8 +4,12 @@ class Solution:
     @return: nothing
     """
     def solveSudoku(self, board):
-        r = [set()] * 9
-        c = [set()] * 9
+        r = [0] * 9
+        for i in range(9):
+            r[i] = set()
+        c = [0] * 9
+        for i in range(9):
+            c[i] = set()
         b = [[0] * 3  for _ in range(3)]
         for i in range(3):
             for j in range(3):
@@ -21,8 +25,8 @@ class Solution:
                         r[i].add(val)
                     if val not in c[j]:
                         c[j].add(val)
-                    if val not in b[i%3][j%3]:
-                        b[i%3][j%3].add(val)
+                    if val not in b[i//3][j//3]:
+                        b[i//3][j//3].add(val)
 
         self.dfs(board, r, c, b, cnt)
         return
@@ -33,16 +37,15 @@ class Solution:
         
         for i in range(9):
             for j in range(9):
-
+                if board[i][j] != 0:
+                    continue
                 for d in range(1, 10):
-                    if board[i][j] != 0:
-                        continue
-                    if d in r[i] or d in c[j] or d in b[i%3][j%3]:
+                    if d in r[i] or d in c[j] or d in b[i//3][j//3]:
                         continue
 
                     r[i].add(d)
                     c[j].add(d)
-                    b[i%3][j%3].add(d)
+                    b[i//3][j//3].add(d)
                     board[i][j] = d
                     cnt -= 1
                     if self.dfs(board, r,c,b, cnt):
@@ -51,6 +54,6 @@ class Solution:
                     cnt += 1
                     r[i].remove(d)
                     c[j].remove(d)
-                    b[i%3][j%3].remove(d)
+                    b[i//3][j//3].remove(d)
                     
         return False
